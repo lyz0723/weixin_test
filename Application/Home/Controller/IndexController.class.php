354ae4bc7f1da9d45a1b0446679376a2efebb08a
weixin <360196045@qq.com>
2017-11-24 16:17:42 +0800

    weiixn7

--- Application/Home/Controller/IndexController.class.php
+++ Application/Home/Controller/IndexController.class.php
@@ -4,16 +4,39 @@ use Think\Controller;
 class IndexController extends Controller {
     public function index(){
         //获取用户openid
-        Vendor('Weixinpay.Weixinpay');
-        $wxpay=new \Weixinpay();
+       // Vendor('Weixinpay.Weixinpay');
+       // $wxpay=new \Weixinpay();
         // 获取jssdk需要用到的数据
-        $data=$wxpay->getParameters();
+       // $data=$wxpay->getParameters();
         // 将数据分配到前台页面
        /* $assign=array(
             'data'=>json_encode($data)
         );
         $this->assign($assign);*/
-        $this->display();
+      //  $this->display();
         //$this->show('<style type="text/css">*{ padding: 0; margin: 0; } div{ padding: 4px 48px;} body{ background: #fff; font-family: "微软雅黑"; color: #333;font-size:24px} h1{ font-size: 100px; font-weight: normal; margin-bottom: 12px; } p{ line-height: 1.8em; font-size: 36px } a,a:hover{color:blue;}</style><div style="padding: 24px 48px;"> <h1>:)</h1><p>欢迎使用 <b>ThinkPHP</b>！</p><br/>版本 V{$Think.version}</div><script type="text/javascript" src="http://ad.topthink.com/Public/static/client.js"></script><thinkad id="ad_55e75dfae343f5a1"></thinkad><script type="text/javascript" src="http://tajs.qq.com/stats?sId=9347272" charset="UTF-8"></script>','utf-8');
+        $redirect_uri=U('Index/index','','',true);
+        $redirect_uri=urlencode($redirect_uri);
+        $url='https://open.weixin.qq.com/connect/oauth2/authorize?appid=wx4ff19b6511cb0168&redirect_uri='.$redirect_uri.'&response_type=
+        code&scope=snsapi_userinfo&state=STATE#wechat_redirect';
+        // curl获取prepay_id
+        $result=$this->curl_get_contents($url);
+        print_r($result);
+    }
+    /**
+     * curl 请求http
+     */
+    public function curl_get_contents($url){
+        $ch=curl_init();
+        curl_setopt($ch, CURLOPT_URL, $url);                //设置访问的url地址
+        // curl_setopt($ch,CURLOPT_HEADER,1);               //是否显示头部信息
+        curl_setopt($ch, CURLOPT_TIMEOUT, 5);               //设置超时
+        curl_setopt($ch, CURLOPT_USERAGENT, $_SERVER['HTTP_USER_AGENT']);   //用户访问代理 User-Agent
+        curl_setopt($ch, CURLOPT_REFERER,$_SERVER['HTTP_HOST']);        //设置 referer
+        curl_setopt($ch,CURLOPT_FOLLOWLOCATION,1);          //跟踪301
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);        //返回结果
+        $r=curl_exec($ch);
+        curl_close($ch);
+        return $r;
     }
 }
