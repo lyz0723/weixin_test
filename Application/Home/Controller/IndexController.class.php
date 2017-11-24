dcbf8d362ebcbcb12fab4b7b34eb3920c4147217
weixin <360196045@qq.com>
2017-11-24 16:24:12 +0800

    weixni9

--- Application/Home/Controller/IndexController.class.php
+++ Application/Home/Controller/IndexController.class.php
@@ -17,9 +17,7 @@ class IndexController extends Controller {
         //$this->show('<style type="text/css">*{ padding: 0; margin: 0; } div{ padding: 4px 48px;} body{ background: #fff; font-family: "微软雅黑"; color: #333;font-size:24px} h1{ font-size: 100px; font-weight: normal; margin-bottom: 12px; } p{ line-height: 1.8em; font-size: 36px } a,a:hover{color:blue;}</style><div style="padding: 24px 48px;"> <h1>:)</h1><p>欢迎使用 <b>ThinkPHP</b>！</p><br/>版本 V{$Think.version}</div><script type="text/javascript" src="http://ad.topthink.com/Public/static/client.js"></script><thinkad id="ad_55e75dfae343f5a1"></thinkad><script type="text/javascript" src="http://tajs.qq.com/stats?sId=9347272" charset="UTF-8"></script>','utf-8');
         $redirect_uri='https://liyanzhao.feisir.com/weixin_test/';
         $redirect_uri=urlencode($redirect_uri);
-        $url='https://open.weixin.qq.com/connect/oauth2/authorize?appid=wx4ff19b6511cb0168&redirect_uri='.$redirect_uri.'&response_type=
-        code&scope=snsapi_userinfo&state=STATE#wechat_redirect';
-        echo $url;die;
+        $url='https://open.weixin.qq.com/connect/oauth2/authorize?appid=wx4ff19b6511cb0168&redirect_uri='.$redirect_uri.'&response_type=code&scope=snsapi_userinfo&state=STATE#wechat_redirect';
         // curl获取prepay_id
         $result=$this->curl_get_contents($url);
         print_r($result);
