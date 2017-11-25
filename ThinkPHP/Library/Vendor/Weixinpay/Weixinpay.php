9c8d45b86511ea0bd1971d5b65baf52cecde0868
lyz0723 <360196045@qq.com>
2017-11-26 01:13:26 +0800

    cc

--- ThinkPHP/Library/Vendor/Weixinpay/Weixinpay.php
+++ ThinkPHP/Library/Vendor/Weixinpay/Weixinpay.php
@@ -188,7 +188,7 @@ class Weixinpay {
             $order=array(
                 'body'=>'test',// 商品描述（需要根据自己的业务修改）
                 'total_fee'=>1,// 订单金额  以(分)为单位（需要根据自己的业务修改）
-                'out_trade_no'=>$out_trade_no,// 订单号（需要根据自己的业务修改）
+                'out_trade_no'=>time(),// 订单号（需要根据自己的业务修改）
                 'product_id'=>'1',// 商品id（需要根据自己的业务修改）
                 'trade_type'=>'JSAPI',// JSAPI公众号支付
                 'openid'=>$openid// 获取到的openid
