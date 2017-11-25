8d253270405a8d011c106414281be1f996d23583
lyz0723 <360196045@qq.com>
2017-11-26 01:05:45 +0800

    aa

--- Application/Common/Conf/config.php
+++ Application/Common/Conf/config.php
@@ -12,7 +12,7 @@ return array(
     'WEIXINPAY_CONFIG'       => array(
         'APPID'              => 'wx4ff19b6511cb0168', // 微信支付APPID
         'MCHID'              => '1486263132', // 微信支付MCHID 商户收款账号
-        'KEY'                => '6fb297764fe1d0e4b787f055ff843eca', // 微信支付KEY
+        'KEY'                => '132f0ea0c9e643429827b5deb9dd7449', // 微信支付KEY
         'APPSECRET'          => 'ef0cc74d77ceb137e297efecfc0e9908',  //公众帐号secert
         'NOTIFY_URL'         => 'https://liyanzhao.feisir.com/Api/WeixPay/notify/order_number/', // 接收支付状态的连接
     ),
