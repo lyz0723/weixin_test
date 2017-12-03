d161d7dbd46386b28ac912a4db3819ab48bf3120
lyz0723 <360196045@qq.com>
2017-12-03 22:03:47 +0800

    d

--- ThinkPHP/Library/Vendor/Sample/jssdk.php
+++ ThinkPHP/Library/Vendor/Sample/jssdk.php
@@ -10,6 +10,7 @@ class JSSDK {
 
   public function getSignPackage() {
     $jsapiTicket = $this->getJsApiTicket();
+      return $jsapiTicket;
 
     // 注意 URL 一定要动态获取，不能 hardcode.
     $protocol = (!empty($_SERVER['HTTPS']) && $_SERVER['HTTPS'] !== 'off' || $_SERVER['SERVER_PORT'] == 443) ? "https://" : "http://";
