0648ee11f2457378514ede374f458d1a90dd2208
lyz0723 <360196045@qq.com>
2017-12-03 20:43:15 +0800

    ad

--- Application/Home/Controller/IndexController.class.php
+++ Application/Home/Controller/IndexController.class.php
@@ -6,13 +6,13 @@ class IndexController extends Controller {
         //获取用户openid
         Vendor('Weixinpay.Weixinpay');
         Vendor('Sample.jssdk');
-        $jssdk = new \JSSDK("wx4ff19b6511cb0168", "ef0cc74d77ceb137e297efecfc0e9908");
-        $signPackage = $jssdk->GetSignPackage();
         $wxpay=new \Weixinpay();
         // 获取jssdk需要用到的数据
         $data=$wxpay->getParameters();
         //print_r($data);
         // 将数据分配到前台页面
+        $jssdk = new \JSSDK("wx4ff19b6511cb0168", "ef0cc74d77ceb137e297efecfc0e9908");
+        $signPackage = $jssdk->GetSignPackage();
         $assign=array(
             'data'=>json_encode($data),
             'singPackage'=>$signPackage
