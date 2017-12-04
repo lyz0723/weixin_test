791d08fe90fb9125a670eb8065debd783d0c7aee
lyz0723 <360196045@qq.com>
2017-12-04 21:57:45 +0800

    dd

--- ThinkPHP/Library/Vendor/Weixinpay/Weixinpay.php
+++ ThinkPHP/Library/Vendor/Weixinpay/Weixinpay.php
@@ -146,9 +146,9 @@ class Weixinpay {
         //签名步骤二：在string后加入KEY
         $config=$this->config;
         $string_sign_temp=$string_a."&key=".$config['KEY'];
-        return $string_sign_temp;
         //签名步骤三：MD5加密
         $sign = md5($string_sign_temp);
+        return $sign;
         // 签名步骤四：所有字符转为大写
         $result=strtoupper($sign);
         return $result;
