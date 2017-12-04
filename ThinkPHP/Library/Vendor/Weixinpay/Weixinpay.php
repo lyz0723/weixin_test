ee98be23e910e9ec312e7785779183563a1928d8
weixin <360196045@qq.com>
2017-12-04 11:19:18 +0800

    d

--- ThinkPHP/Library/Vendor/Weixinpay/Weixinpay.php
+++ ThinkPHP/Library/Vendor/Weixinpay/Weixinpay.php
@@ -137,7 +137,7 @@ class Weixinpay {
      */
     public function makeSign($data){
         // 去空
-        $data=array_filter($data);
+        /*$data=array_filter($data);
         //签名步骤一：按字典序排序参数
         ksort($data);
         $string_a=http_build_query($data);
@@ -150,7 +150,24 @@ class Weixinpay {
         $sign = md5($string_sign_temp);
         // 签名步骤四：所有字符转为大写
         $result=strtoupper($sign);
-        return $result;
+        return $result;*/
+
+        array_filter($data);
+        if(isset($arr['paySign'])){
+            unset($arr['paySign']);
+        }
+        $config=$this->config;
+        //排序
+        ksort($arr);
+        //组装字符
+        $str = $this->arrToUrl($arr) . '&key='.$config['KEY'];
+        //使用md5 加密 转换成大写
+        return strtoupper(md5($str));
+    }
+
+    //数组转URL字符串 不带key
+    public function arrToUrl($arr){
+        return urldecode(http_build_query($arr));
     }
 
     /**
